9ca75d34a1633c9bf6ca060da653ac0fddfefc05
dvyns <dvyn@berkeley.edu>
2026-04-30 14:36:02 -0700

    improved rendering time with moller trumbore

--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -28,24 +28,29 @@ bool Triangle::has_intersection(const Ray &r) const {
   // function records the "intersection" while this function only tests whether
   // there is a intersection.
   
-  Matrix3x3 system(-r.d.x, p2.x - p1.x, p3.x - p1.x,
-                    -r.d.y, p2.y - p1.y, p3.y - p1.y,
-                    -r.d.z, p2.z - p1.z, p3.z - p1.z);
-  
-  Vector3D intersection_params = system.inv() * (r.o - p1);
-  
-  double distance = intersection_params.x;
-  double u_bary = intersection_params.y;
-  double v_bary = intersection_params.z;
-  
-  bool in_ray_range = (distance >= r.min_t) && (distance <= r.max_t);
-  bool valid_bary = (u_bary >= 0) && (v_bary >= 0) && ((u_bary + v_bary) <= 1) && (u_bary <= 1) && (v_bary <= 1);
-  
-  if (!in_ray_range || !valid_bary) {
-    return false;
+  const Vector3D E1 = p2 - p1;
+  const Vector3D E2 = p3 - p1;
+  const Vector3D S = r.o - p1;
+  const Vector3D S1 = cross(r.d, E2);
+  const Vector3D S2 = cross(S, E1);
+  const double denom = dot(E1, S1);
+  if (fabs(denom) < EPS_D) return false;
+
+  const double b_1_num = dot(S, S1);
+  const double b_2_num = dot(r.d, S2);
+  const double t_num = dot(E2, S2);
+  const double edge_eps = EPS_D * fabs(denom);
+
+  if (denom > 0.0) {
+    if (b_1_num < -edge_eps || b_1_num > denom + edge_eps) return false;
+    if (b_2_num < -edge_eps || b_1_num + b_2_num > denom + edge_eps) return false;
+    if (t_num < r.min_t * denom || t_num > r.max_t * denom) return false;
+  } else {
+    if (b_1_num > edge_eps || b_1_num < denom - edge_eps) return false;
+    if (b_2_num > edge_eps || b_1_num + b_2_num < denom - edge_eps) return false;
+    if (t_num > r.min_t * denom || t_num < r.max_t * denom) return false;
   }
-  
-  r.max_t = distance;
+
   return true;
 
 }
@@ -55,31 +60,47 @@ bool Triangle::intersect(const Ray &r, Intersection *isect) const {
   // implement ray-triangle intersection. When an intersection takes
   // place, the Intersection data should be updated accordingly
   
-  Matrix3x3 coeff_matrix(-r.d.x, p2.x - p1.x, p3.x - p1.x,
-                         -r.d.y, p2.y - p1.y, p3.y - p1.y,
-                         -r.d.z, p2.z - p1.z, p3.z - p1.z);
-  
-  Vector3D params = coeff_matrix.inv() * (r.o - p1);
-  
-  double ray_t = params.x;
-  double bary_u = params.y;
-  double bary_v = params.z;
-  double bary_w = 1.0 - bary_u - bary_v;
-  
-  if (ray_t < r.min_t || ray_t > r.max_t) {
-    return false;
+  const Vector3D E1 = p2 - p1;
+  const Vector3D E2 = p3 - p1;
+  const Vector3D S = r.o - p1;
+  const Vector3D S1 = cross(r.d, E2);
+  const Vector3D S2 = cross(S, E1);
+  const double denom = dot(E1, S1);
+  if (fabs(denom) < EPS_D) return false;
+
+  // switched to using doubles because I thought that was causing the precision error
+  // not sure if it was this or just the division problem, 
+  // but performance seems fine with doubles so I figure we keep them
+  const double b_1_num = dot(S, S1);
+  const double b_2_num = dot(r.d, S2);
+  const double t_num = dot(E2, S2);
+  const double edge_eps = EPS_D * fabs(denom);
+
+  // weird artifacts showed up on bigger .dae files with small/dense triangle mesheswhen I divided by denom before comparisons
+  // that suggested some kind of precision error so we perform the same comparison without division
+  if (denom > 0.0) {
+    if (b_1_num < -edge_eps || b_1_num > denom + edge_eps) return false;
+    if (b_2_num < -edge_eps || b_1_num + b_2_num > denom + edge_eps) return false;
+    if (t_num < r.min_t * denom || t_num > r.max_t * denom) return false;
+  } else {
+    if (b_1_num > edge_eps || b_1_num < denom - edge_eps) return false;
+    if (b_2_num > edge_eps || b_1_num + b_2_num < denom - edge_eps) return false;
+    if (t_num > r.min_t * denom || t_num < r.max_t * denom) return false;
   }
-  
-  if (bary_u < 0 || bary_u > 1 || bary_v < 0 || bary_v > 1 || bary_w < 0 || bary_w > 1) {
-    return false;
-  }
-  
-  r.max_t = ray_t;
-  isect->t = ray_t;
-  isect->n = bary_w * n1 + bary_u * n2 + bary_v * n3;
+
+  const double inv_denom = 1.0 / denom;
+  const double t = t_num * inv_denom;
+  const double b_1 = b_1_num * inv_denom;
+  const double b_2 = b_2_num * inv_denom;
+  const Vector3D n = ((1.0 - b_1 - b_2) * n1 + b_1 * n2 + b_2 * n3).unit();
+
+  isect->t = t;
+  isect->n = n;
   isect->primitive = this;
-  isect->bsdf = get_bsdf();
+  isect->bsdf = this->get_bsdf();
   
+  r.max_t = isect->t;
+
   return true;
 }
 
